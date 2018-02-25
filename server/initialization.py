05968cea2a824147f0a0e4d8038524c9df502eba
mepihindeveloper <mepihindeveloper@gmail.com>
2018-02-26 01:56:15 +0300

    Получение первичные 3 цвета картинки, анализ длины волос, классовая структура приложения, добавления данных в бд

--- server/initialization.py
+++ server/initialization.py
@@ -5,10 +5,6 @@ import random
 import cv2
 import numpy as np
 
-path_to_hairstyles = './hairstyle_images'
-path_to_men_hairstyles = "/men"
-path_to_women_hairstyles = "/women"
-
 hairstyle_templates = {
     'men': {
         'templates': {}
@@ -17,7 +13,6 @@ hairstyle_templates = {
         'templates': {}
     }
 }
-
 hair_length_associate = {
     'short': {
         'min': 10,
@@ -33,109 +28,186 @@ hair_length_associate = {
     }
 }
 
-def initialization():
-    # Установка соединения и назначение курсора
-    connection = sqlite3.connect('./database/main_db.db')
-    cursor = connection.cursor()
 
-    cursor.execute("DELETE FROM hairstyle_images")
+class Initialization:
+    # Параметры системы инициализации
+    params = {
+        'path_to_hairstyles': './hairstyle_images',
+        'path_to_men_hairstyles': "/men",
+        'path_to_women_hairstyles': "/women",
+        'hair_types': ["normal", "greasy", "dry", "mixed"],
+        'debug': False
+    }
 
-    print("Сканирование женских шаблонов...")
+    connection = cursor = None
 
-    dir_and_subdir_templates = [
-        os.path.join(path_to_hairstyles+path_to_women_hairstyles, name)
-        for dir_path, dirs, files in os.walk(path_to_hairstyles+path_to_women_hairstyles)
-        for name in files
-        if name.endswith((".jpg", ".png", ".jpeg"))
-    ]
+    def __init__(self, params={}):
+        for param in params:
+            if param is not None:
+                self.params.update({param: params.get(param)})
 
-    print(dir_and_subdir_templates)
+    # Функция уставновки соединения с БД
+    def __init_connection(self):
+        self.connection = sqlite3.connect('./database/main_db.db')
+        self.cursor = self.connection.cursor()
 
+    # Функция разрыва соединения
+    def __exit_connection(self):
+        self.cursor.close()
+        self.connection.close()
 
-    hair_types = ["normal", "greasy", "dry", "mixed"]
-    i = 0
-    for template in dir_and_subdir_templates:
-        #hair_type = input("Введите тип волос (normal, greasy, dry, mixed): ")
-        #while hair_type not in hair_types:
-        #    print("Ошибка: вы можете ввести только значения: normal, greasy, dry, mixed")
-        #    hair_type = input("Введите тип волос (normal, greasy, dry, mixed): ")
-        hair_type = random.choice(hair_types)
-        length = detect_hair_length(template)
-        color = detect_primary_color(template)
-        new_template = {
-            'item_{}'.format(i): {
-                'path': template,
-                'type': hair_type,
-                'length': length,
-                'color': color
-            }
-        }
+    # Основная функция инициализации
+    def initialization(self):
+        # Установка соединения и назначение курсора
+        self.__init_connection()
 
-        hairstyle_templates.get('women').get('templates').update(new_template)
-        i += 1
+        # Очищаем базу данных от возмжных записей (предусматривается, что их не должно быть)
+        self.cursor.execute("DELETE FROM hairstyle_images")
 
-    for item in hairstyle_templates.get('women').get('templates'):
-        cursor.execute("INSERT INTO hairstyle_images "
-                       "(path_to_image, hair_type, hair_length, hair_color, gender) "
-                       "VALUES ('%s', '%s', '%s', '%s','%s')" % (
+        # Получаем все картинки из выбранной папки
+        print("Сканирование женских шаблонов...")
+        dir_and_subdir_templates = [
+            os.path.join(self.params.get('path_to_hairstyles') + self.params.get('path_to_women_hairstyles'), name)
+            for dir_path, dirs, files in os.walk(self.params.get('path_to_hairstyles') + self.params.get('path_to_women_hairstyles'))
+            for name in files
+            if name.endswith((".jpg", ".png", ".jpeg"))
+        ]
+
+        if self.params.get('debug') is True:
+            print(dir_and_subdir_templates)
+
+        # Генерируем структуры для шаблонов причесок
+        self.__generate_hairstyle_structure(dir_and_subdir_templates)
+
+        # Добавляем новые строки в базу данных
+        self.__insert_into_db(self.connection, self.cursor)
+
+        # Разрыв соединения с БД
+        self.__exit_connection()
+
+    # Функция добавления новых записей в базу данных
+    def __insert_into_db(self, connection, cursor):
+        '''
+
+        :param connection: Активное соединение с базой данных
+        :param cursor: Курсор управления запросами и БД
+        :return:
+        '''
+        global hairstyle_templates
+        for item in hairstyle_templates.get('women').get('templates'):
+            cursor.execute("INSERT INTO hairstyle_images "
+                           "(path_to_image, hair_type, hair_length, hair_color, gender) "
+                           "VALUES ('%s', '%s', '%s', '%s','%s')" % (
                                hairstyle_templates.get('women').get('templates').get(item)['path'],
                                hairstyle_templates.get('women').get('templates').get(item)['type'],
                                hairstyle_templates.get('women').get('templates').get(item)['length'],
                                hairstyle_templates.get('women').get('templates').get(item)['color'],
                                'women'
                            )
-                       )
-        connection.commit()
+                           )
+            connection.commit()
 
+    # Функция генерации структуры для шаблонов причесок
+    def __generate_hairstyle_structure(self, dir_and_subdir_templates):
+        global hairstyle_templates
+        i = 0
+        '''
+            Цикл прхода по всем шаблонам из найденных в dir_and_subdir_templates
+            Идет считываение типа волос путем ввода вручную
+            Процесс определения длины волос напрямую зависит от высоты фото (условие задачи и ее рациональность)
+            Процесс получения лидирующего цвета по HSV (для точности выделяется 3 цвета)
+            Формируется новый словарь по шаблону
+            Добавляется в общую структуру
+        '''
+        for template in dir_and_subdir_templates:
+            # hair_type = input("Введите тип волос (normal, greasy, dry, mixed): ")
+            # while hair_type not in self.params.get('hair_types'):
+            #    print("Ошибка: вы можете ввести только значения: normal, greasy, dry, mixed")
+            #    hair_type = input("Введите тип волос (normal, greasy, dry, mixed): ")
 
-def detect_hair_length(single_template):
-    single_template = cv2.imread(single_template)
-    length = None
-    for item in hair_length_associate:
-        if hair_length_associate.get(item)['min'] <= int(single_template.shape[0]) <= hair_length_associate.get(item)['max']:
-            length = item
-    return length
+            hair_type = random.choice(self.params.get('hair_types'))
+            length = self.__detect_hair_length(template)
+            color = self.__detect_primary_colors(template)[0]
+            new_template = {
+                'item_{}'.format(i): {
+                    'path': template,
+                    'type': hair_type,
+                    'length': length,
+                    'color': color
+                }
+            }
 
-def detect_primary_color(single_template):
-    pass
+            hairstyle_templates.get('women').get('templates').update(new_template)
+            i += 1
 
-def remove_background():
-    dir_and_subdir_templates = convert_to_png()
+    # Функция определения длины волос
+    def __detect_hair_length(self, single_template):
+        '''
 
-    for template in dir_and_subdir_templates:
-        src = cv2.imread(template, 1)
-        gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+        :param single_template: Путь к изображнию шаблона для анализа
+        :return: Длина волос
+        '''
+        global hair_length_associate
+        single_template = cv2.imread(single_template)
+        length = None
 
-        _, alpha = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY)
-        b, g, r = cv2.split(src)
-        rgba = [b, g, r, alpha]
-        dst = cv2.merge(rgba, 4)
+        # Задается математическое выражение, что если длина >= минимальной и длина <= максимальной
+        for item in hair_length_associate:
+            if hair_length_associate.get(item)['min'] <= int(single_template.shape[0]) <= \
+                    hair_length_associate.get(item)['max']:
+                length = item
+        return length
 
-        cv2.imwrite(template, dst)
+    # Функция опредеоения лидирующих цветов
+    def __detect_primary_colors(self, single_template):
+        '''
 
+        :param single_template: Путь к изображнию шаблона для анализа
+        :return: Массив трех лидируюших цветов
+        '''
+        single_photo = cv2.imread(single_template)
+        hue_color_name = [
+            [355, 360, "Red"],
+            [346, 355, "Pink-Red"],
+            [331, 345, "Pink"],
+            [321, 330, "Magenta-Pink"],
+            [281, 320, "Magenta"],
+            [241, 280, "Blue-Magenta"],
+            [221, 240, "Blue"],
+            [201, 220, "Cyan-Blue"],
+            [170, 200, "Cyan"],
+            [141, 169, "Green-Cyan"],
+            [81, 140, "Green"],
+            [61, 80, "Yellow-Green"],
+            [51, 60, "Yellow"],
+            [41, 50, "Orange-Yellow"],
+            [21, 40, "Orange-Brown"],
+            [11, 20, "Red-Orange"],
+            [1, 10, "Red"]
+        ]
 
-def convert_to_png():
-    dir_and_subdir_templates = [
-        os.path.join(path_to_hairstyles + path_to_women_hairstyles, name)
-        for dir_path, dirs, files in os.walk(path_to_hairstyles + path_to_women_hairstyles)
-        for name in files
-        if name.endswith((".jpg", ".png", ".jpeg"))
-    ]
+        '''
+            Конвертация в HSV для работы
+            Нарезка HSV на составляющие hue, saturation, value
+            Постороение гистограммы по модели HSV
+        '''
+        hsv = cv2.cvtColor(single_photo, cv2.COLOR_BGR2HSV)
+        hue, s, v = cv2.split(hsv)
+        hue_hist, bins = np.histogram(hue, 360)
+        hues = {}
 
-    for template in dir_and_subdir_templates:
-        src = cv2.imread(template)
+        i = 0
+        while i < len(hue_color_name):
+            if hue_color_name[i][2] in hues:
+                hues[hue_color_name[i][2]] = hues.get(hue_color_name[i][2]) + np.sum(
+                    hue_hist[hue_color_name[i][0]:hue_color_name[i][1]])
+            else:
+                hues[hue_color_name[i][2]] = (np.sum(hue_hist[hue_color_name[i][0]:hue_color_name[i][1]]))
+            i += 1
 
-        cv2.imwrite(template[:-3] + 'png', src)
-        os.remove(template)
-
-    dir_and_subdir_templates = [
-        os.path.join(path_to_hairstyles + path_to_women_hairstyles, name)
-        for dir_path, dirs, files in os.walk(path_to_hairstyles + path_to_women_hairstyles)
-        for name in files
-        if name.endswith(".png")
-    ]
-
-    return dir_and_subdir_templates
+        s_hues = sorted(hues, key=hues.get, reverse=True)
+        return [s_hues[0], s_hues[1], s_hues[2]]
 
 if __name__ == "__main__":
-    initialization()
+    initialization = Initialization().initialization()
+
